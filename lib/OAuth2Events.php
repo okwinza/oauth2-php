032f3cf1dbfd3b335668a4f7d8c15a156d8db12f
Okwinza <okwinza@gmail.com>
2019-07-01 12:45:54 +0300

    Simple EventDispatcher integration

---
+++ lib/OAuth2Events.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OAuth2;
+
+namespace OAuth2;
+
+use OAuth2\Event\PreGrantAccessTokenEvent;
+
+class OAuth2Events
+{
+    /**
+     * The PRE_GRANT_ACCESS_TOKEN event occurs before the access token generation.
+     *
+     * This allows to modify the grant access token parameters
+     *
+     * @Event(PreGrantAccessTokenEvent::class)
+     * @var string
+     */
+    const PRE_GRANT_ACCESS_TOKEN   = "oauth2.pre.grant.access_token";
+}
